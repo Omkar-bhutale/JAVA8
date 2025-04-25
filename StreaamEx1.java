7c2d2e0244cc66cbccee7f035982cda66673bb30
Bhutale Omkar Pavankumar <bhutaleomkar95@gmail.com>
2025-04-25 10:16:14 +0530

    Create StreaamEx1.java

---
+++ StreaamEx1.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class StreaamEx1 {
+    public static void main(String[] args) {
+        // Write a Java program to calculate the average of a list of integers using streams.
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+        
+
+    }
+}
